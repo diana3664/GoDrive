717d86388fb95ae940ba27614947842d7a0e65e6
diana3664 <dianamus2017@gmail.com>
2021-06-22 09:55:38 +0300

    create our ratings constractor

--- src/main/java/models/Ratings.java
+++ src/main/java/models/Ratings.java
@@ -1,4 +1,13 @@
 package models;
 
 public class Ratings {
+
+    private int driverId;
+    private String comment;
+    private int ratings;
+    private int id;
+
+    public Ratings (int driverid, String comment, int ratings){
+
+    }
 }
