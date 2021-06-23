426bf94d1a52f3fb18a0fcbbb291cc228282aa1c
diana3664 <dianamus2017@gmail.com>
2021-06-23 21:59:05 +0300

    add rout to our form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import models.Ratings;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -19,6 +20,27 @@ public class App {
             Map<String,Object> model = new HashMap<>();
             return new ModelAndView(model,"login.hbs");
         },new HandlebarsTemplateEngine());
+
+
+        get("/rating/create", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "ratings-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        post("/rating/create/new", (request,response)-> {
+            Map<String, Object> model = new HashMap<>();
+
+
+            String name = request.queryParams("name");
+            String comment = request.queryParams("comment");
+            int review =Integer.parseInt(request.queryParams("3")) ;
+            Ratings ratings = new Ratings(name,comment,3);
+            ratings.save();
+            model.put("ratings", ratings);
+
+            return new ModelAndView(model, "successRatings.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 }
